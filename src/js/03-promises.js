c5b33ec89571cef70de0d3017a6f56640b5a56ba
KateSlav7 <kateslavgorodskaya@gmail.com>
2021-11-29 12:41:52 +0200

    commit_js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,14 +2,12 @@ import Notiflix from 'notiflix';
 function createPromise(position, delay) {
 const shouldResolve = Math.random() > 0.3;
 if (shouldResolve) {
-   
 return new Promise((resolve, reject) => {
 setTimeout(() => {
 resolve({ position, delay });
 }, delay);
 });
 } else {
-   
 return new Promise((resolve, reject) => {
 setTimeout(() => {
 reject({ position, delay });
